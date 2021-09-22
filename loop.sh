94349ed0c65abdc6d21c6240459641d4b62729a1
Charlie Root <root@cbsdbuilder122.my.domain>
2021-09-22 15:59:46 +0300

    wip

--- loop.sh
+++ loop.sh
@@ -8,21 +8,21 @@ set -e
 . ${MYDIR}/func.subr
 set +e
 
-#${MYDIR}/base.sh -v ${ver} -a ${arch} -t ${target_arch}
-#ret=$?
+${MYDIR}/base.sh -v ${ver} -a ${arch} -t ${target_arch}
+ret=$?
 
-#if [ ${ret} -ne 0 ]; then
-#	echo "error: ${MYDIR}/base.sh -v ${ver} -a ${arch} -t ${target_arch}"
-#	exit ${ret}
-#fi
+if [ ${ret} -ne 0 ]; then
+	echo "error: ${MYDIR}/base.sh -v ${ver} -a ${arch} -t ${target_arch}"
+	exit ${ret}
+fi
 
-#${MYDIR}/kernel.sh -v ${ver} -a ${arch} -t ${target_arch}
-#ret=$?
+${MYDIR}/kernel.sh -v ${ver} -a ${arch} -t ${target_arch}
+ret=$?
 
-#if [ ${ret} -ne 0 ]; then
-#	echo "error: ${MYDIR}/kernel.sh -v ${ver} -a ${arch} -t ${target_arch}"
-#	exit ${ret}
-#fi
+if [ ${ret} -ne 0 ]; then
+	echo "error: ${MYDIR}/kernel.sh -v ${ver} -a ${arch} -t ${target_arch}"
+	exit ${ret}
+fi
 
 log=$( mktemp )
 trap "rm -f ${log}" HUP INT ABRT BUS TERM EXIT
